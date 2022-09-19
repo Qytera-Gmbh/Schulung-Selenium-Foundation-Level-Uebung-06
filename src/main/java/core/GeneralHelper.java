cbd19ec574acb0c982545ccbbfc8e652dd6bb967
MoritzSalein <moritz.salein@qytera.de>
2022-09-19 16:56:23 +0200

    mit OS Auswahl

--- src/main/java/core/GeneralHelper.java
+++ src/main/java/core/GeneralHelper.java
@@ -20,25 +20,36 @@ public class GeneralHelper {
     public GeneralPage generalPage = GeneralPage.getInstance();
 
     public static WebDriver getDriver(){
+        var lxResourcePath = "src/test/resources/chromedriver";
+        var operatingSystem = System.getProperty("os.name").toLowerCase();
+
         if(driver==null) {
-            if(System.getenv("SELENIUM_BROWSER_CONFIG")!= null){
-                if(System.getenv("SELENIUM_BROWSER_CONFIG").equalsIgnoreCase("chrome")) {
-                    System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+            if(System.getenv("SELENIUM_BROWSER_CONFIG") != null){
+                if(System.getenv("SELENIUM_BROWSER_CONFIG").equalsIgnoreCase("chrome") && operatingSystem.contains("windows")) {
+                    System.setProperty("webdriver.chrome.driver", lxResourcePath +".exe");
                     return new ChromeDriver();
                 }
-                if(System.getenv("SELENIUM_BROWSER_CONFIG").equalsIgnoreCase("firefox")) {
-                    System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\geckodriver.exe");
+                if(System.getenv("SELENIUM_BROWSER_CONFIG").equalsIgnoreCase("chrome") && operatingSystem.contains("linux")) {
+                    System.setProperty("webdriver.chrome.driver", lxResourcePath);
+                    return new ChromeDriver();
+                }
+                if(System.getenv("SELENIUM_BROWSER_CONFIG").equalsIgnoreCase("firefox") && operatingSystem.contains("windows")) {
+                    System.setProperty("webdriver.gecko.driver", lxResourcePath.replace("chrome","gecko") + ".exe");
+                    return new FirefoxDriver();
+                }
+                if(System.getenv("SELENIUM_BROWSER_CONFIG").equalsIgnoreCase("firefox") && operatingSystem.contains("linux")) {
+                    System.setProperty("webdriver.gecko.driver", lxResourcePath.replace("chrome","gecko"));
                     return new FirefoxDriver();
                 }
                 else{
                     //Default
-                    System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+                    System.setProperty("webdriver.chrome.driver", lxResourcePath + ".exe");
                     return new ChromeDriver();
                 }
             }
             else {
                 //Default
-                System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", lxResourcePath);
                 return new ChromeDriver();
             }
         }
