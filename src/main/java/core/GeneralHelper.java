5e6cb1adfef44ed7104523d580d21cb1df40d2b0
MoritzSalein <moritz.salein@qytera.de>
2022-09-21 15:59:56 +0200

    Update tearDown

--- src/main/java/core/GeneralHelper.java
+++ src/main/java/core/GeneralHelper.java
@@ -62,7 +62,9 @@ public class GeneralHelper {
 
     @AfterEach
     public void tearDown(){
-        driver.quit();
-        driver=null;
+        if(driver!=null){
+            driver.quit();
+            driver=null;
+        }
     }
 }
